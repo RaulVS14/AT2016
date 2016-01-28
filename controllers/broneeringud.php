e1c3fa267cf29a783e26948fcd57079bae7d90fb
Raul <raul.muser@gmail.com>
2016-01-28 10:28:55 +0200

    Täienda kontrollerit

---
+++ controllers/broneeringud.php
@@ -0,0 +1,44 @@
+<?php namespace Halo;
+
+/**
+ * Created by PhpStorm.
+ * User: Maile
+ * Date: 17.09.14
+ * Time: 18:19
+ */
+class broneeringud extends Controller
+{
+
+    function index()
+    {
+
+
+    }
+
+    function view()
+    {
+
+
+    }
+
+    function index_post()
+    {
+
+    }
+
+    function edit_post()
+    {
+
+    }
+
+    function delete_post()
+    {
+
+    }
+
+    function edit()
+    {
+
+    }
+
+} 
