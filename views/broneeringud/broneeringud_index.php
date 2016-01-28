94dd5a00cfa6b683431f076dbfffb692483ecb1d
riivostolts <riivostolts@live.com>
2016-01-28 12:30:13 +0200

    [Delivers #112576227]

---
+++ views/broneeringud/broneeringud_index.php
@@ -0,0 +1,112 @@
+<style>
+    .btn {
+        margin-top:20px;
+        margin-right:15px;
+    }
+
+
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <form class="well" role="form">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="contact-name">Eesnimi</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="contact-name">Perenimi</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="email">E-post</label>
+                            <input type="email" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="telefon">Telefon</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                        <br>
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="contact-name">Kuupäev</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="sel1">Vali kellaaeg</label>
+                            <select class="form-control">
+                                <option>Vali</option>
+                                <option>11:30</option>
+                                <option>11:45</option>
+                                <option>12:00</option>
+                                <option>12:15</option>
+                                <option>12:30</option>
+                                <option>12:45</option>
+                                <option>13:00</option>
+                            </select>
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="contact-name">Inimeste arv</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <label for="sel1">Eelroog</label>
+                        <select class="form-control">
+                            <option>Jah</option>
+                            <option>Ei</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-4">
+                        <label for="sel1">Pearoog</label>
+                        <select class="form-control">
+                            <option>Jah</option>
+                            <option>Ei</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-4">
+                        <label for="sel1">Magustoit</label>
+                        <select class="form-control">
+                            <option>Jah</option>
+                            <option>Ei</option>
+                        </select>
+                        <br>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label>Eelroogade arv</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label>Pearoogade arv</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label>Magustoitude arv</label>
+                            <input type="text" class="form-control">
+                        </div><!--form-group-->
+                    </div><!--col-->
+                    <input type="submit" class="btn btn-primary pull-right" value="Kinnita">
+                </div><!--row-->
+            </form>
+        </div>
+    </div>
+</div>
